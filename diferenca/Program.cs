5af7717cc26f2e41444ddcf27b89b6d62818ebe8
Usuario <Usuario@INVENT081>
2023-10-30 10:41:16 -0300

    Adicionar arquivos de projeto.

---
+++ diferenca/Program.cs
@@ -0,0 +1,16 @@
+﻿
+class URI
+{
+    static void Main(string[] args)
+    {
+
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int C = int.Parse(Console.ReadLine());
+        int D = int.Parse(Console.ReadLine());
+        int E = (A * B) - (C * D);
+        string dif = "DIFERENCA = ";
+        Console.WriteLine(dif + E);
+    }
+
+}
